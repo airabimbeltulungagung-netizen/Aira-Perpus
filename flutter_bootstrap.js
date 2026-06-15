39340cba1616fd5da5ec8e21a86bb31983bfbf63
airabimbeltulungagung-netizen <airabimbeltulungagung-netizen@users.noreply.github.com>
2026-06-15 03:36:52 +0000

    deploy: da7a65cd03e647acf26d789869880ffccfef4af4

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"77e2e94772b6eb43759e34ed1ad7da4674e19c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1058617401" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2158820411" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
